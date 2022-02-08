419ba9bab59deb13c43b41b61d59a9e654a9187f
Fernando Guerrero <devrrior@gmail.com>
2022-02-07 19:03:46 -0600

    create Poll model

--- apps/polls/models.py
+++ apps/polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Poll(models.Model):
+    created = models.DateField(editable=False)
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(User, self).save(*args, **kwargs)
