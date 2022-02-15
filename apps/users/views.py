40851547018d066bcffaec978c5ceb07bf92423f
Fernando Guerrero <devrrior@gmail.com>
2022-02-15 14:39:44 -0600

    feat(login): add conditional, if user is authenticated redirect to home page

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect
+from django.http.response import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
@@ -16,6 +16,12 @@ class LoginView(FormView):
         kwargs = super().get_form_kwargs()
         kwargs.update({'request': self.request})
         return kwargs
+
+    # If the user is authenticated, then redirect to home page
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return HttpResponseRedirect(self.get_success_url())
+        return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
         login(self.request, form.get_user())
