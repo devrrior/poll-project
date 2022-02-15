212f6c09c7b5758f402d84fbb8cc1a441e80fb41
Fernando Guerrero <devrrior@gmail.com>
2022-02-15 15:28:18 -0600

    feat(settings.py): set up static files

--- config/settings.py
+++ config/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # User model which the project use
 AUTH_USER_MODEL = 'users.User'
 
--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from apps.users.views import LoginView
 from django.contrib.auth.views import LogoutView
@@ -9,4 +11,4 @@ urlpatterns = [
     path('login/', LoginView.as_view(),name = 'login'),
     path('admin/', admin.site.urls),
     path('logout/', LogoutView.as_view(), name = 'logout'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
