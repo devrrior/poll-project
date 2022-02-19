aa54a9cdcbfe8bd0c4ac3bdddd440114a2b8db5a
Fernando Guerrero <devrrior@gmail.com>
2022-02-18 23:33:09 -0600

    feat(polls): add DetailView and add loginmixer

--- apps/polls/urls.py
+++ apps/polls/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from apps.polls.views import PollCreateView, PollDeleteView
+from apps.polls.views import PollCreateView, PollDeleteView, PollDetailView
 
 app_name = 'polls'
 
 urlpatterns = [
     path('dashboard/', PollCreateView.as_view(), name='dashboard'),
     path('<int:pk>/delete', PollDeleteView.as_view(), name='delete'),
+    path('<int:pk>/edit', PollDetailView.as_view(), name='edit'),
 ]
--- apps/polls/views.py
+++ apps/polls/views.py
@@ -1,11 +1,13 @@
 from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from apps.polls.forms import PollCreateForm
 
 from apps.polls.models import Poll
 
 
-class PollCreateView(CreateView):
+class PollCreateView(LoginRequiredMixin, CreateView):
     model = Poll
     form_class = PollCreateForm
     template_name = 'polls/dashboard.html'
@@ -16,8 +18,13 @@ class PollCreateView(CreateView):
         return super().get_context_data(**kwargs)
 
 
-class PollEditView:
-    pass
+class PollDetailView(DetailView):
+    model = Poll
+    template_name = 'polls/edit.html'
+
+    def get(self, request, pk, *args, **kwargs):
+        request.session['poll_id'] = pk
+        return super().get(request, *args, **kwargs)
 
 
 class PollDeleteView(DeleteView):
