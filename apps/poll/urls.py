d8c5f9ea4929fbbfdc928dcb40b77bce69b436a6
Fernando Guerrero <devrrior@gmail.com>
2022-02-19 23:16:09 -0600

    feat(poll/views): create view for publish poll

--- apps/poll/urls.py
+++ apps/poll/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import PollCreateView, PollDeleteView, PollDetailView
+from .views import PollCreateView, PollDeleteView, PollDetailView, PollPublishView
 
 app_name = 'poll'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('dashboard/', PollCreateView.as_view(), name='dashboard'),
     path('<int:pk>/delete', PollDeleteView.as_view(), name='delete'),
     path('<int:pk>/edit', PollDetailView.as_view(), name='edit'),
+    path('<int:pk>/publish', PollPublishView.as_view(), name='publish'),
 ]
 
--- apps/poll/views.py
+++ apps/poll/views.py
@@ -1,4 +1,6 @@
+from django.http.response import HttpResponseRedirect
 from django.urls import reverse_lazy
+from django.views.generic.base import View
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -18,17 +20,31 @@ class PollCreateView(LoginRequiredMixin, CreateView):
         kwargs['object_list'] = Poll.objects.all()
         return super().get_context_data(**kwargs)
 
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
 
-class PollDetailView(DetailView):
+
+class PollDetailView(LoginRequiredMixin,DetailView):
     model = Poll
     template_name = 'poll/edit.html'
 
-    def get(self, request, pk, *args, **kwargs):
-        request.session['poll_id'] = pk
+    def get(self, request, *args, **kwargs):
+        id = self.kwargs.get('pk')
+        request.session['poll_id'] = id
         return super().get(request, *args, **kwargs)
 
 
-class PollDeleteView(DeleteView):
+class PollDeleteView(LoginRequiredMixin, DeleteView):
     model = Poll
     success_url = reverse_lazy('poll:dashboard')
 
+class PollPublishView(View):
+    def get(self, request, *args, **kwargs):
+        id = self.kwargs.get('pk')
+        next = request.GET.get('next', '/')
+        poll_object = Poll.objects.get(id=id)
+        poll_object.status = 'published'
+        poll_object.save()
+        print('todo ok')
+        return HttpResponseRedirect(next)
