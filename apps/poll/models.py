524da03f65538d94f8b205b98c5eea25903fea6d
Fernando Guerrero <devrrior@gmail.com>
2022-02-19 23:15:32 -0600

    feat(poll/models): add status and created_by field

--- apps/poll/models.py
+++ apps/poll/models.py
@@ -1,10 +1,19 @@
 import uuid
 from django.db import models
 
+from apps.user.models import User
+
 
 class Poll(models.Model):
+    STATUS_CHOICES = [
+        ('published','Published'),
+        ('draft', 'Draft'),
+    ]
+
     name = models.CharField(max_length=100)
     code = models.CharField(default=str(uuid.uuid4())[:8], editable=False, max_length=8)
+    status = models.CharField(max_length=9, choices=STATUS_CHOICES, default='draft')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
