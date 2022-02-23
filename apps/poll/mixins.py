3d83c9ce55bfec3d8cebcf5eac22ef674ffe4403
Fernando Guerrero <devrrior@gmail.com>
2022-02-23 00:02:08 -0600

    fix(poll/mixins.py): compare with user not with poll object

--- apps/poll/mixins.py
+++ apps/poll/mixins.py
@@ -1,4 +1,5 @@
 from django.urls import reverse_lazy
+from django.contrib import messages
 from django.http.response import HttpResponseRedirect
 
 from apps.poll.models import Poll
@@ -7,7 +8,9 @@ from apps.poll.models import Poll
 class PollPermissionMixin(object):
     def dispatch(self, request, *args, **kwargs):
         id = self.kwargs.get('pk')
-        if self.request.user != Poll.objects.get(id=id):
+        if self.request.user != Poll.objects.get(id=id).created_by:
             print('no te pertenece estooo!')
+            print('user from self' + self.request.user)
+            messages.add_message(request, messages.INFO, 'This poll does not belong to you')
             return HttpResponseRedirect(reverse_lazy('poll:dashboard'))
         return super().dispatch(request, *args, **kwargs)
