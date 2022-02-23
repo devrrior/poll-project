5c710b5e6ce1cb679ffda674ebc96606bb0c45fe
Fernando Guerrero <devrrior@gmail.com>
2022-02-23 00:33:19 -0600

    feat(question/mixins.py): compare with user not with question object

--- apps/question/mixins.py
+++ apps/question/mixins.py
@@ -1,4 +1,5 @@
 from django.urls import reverse_lazy
+from django.contrib import messages
 from django.http.response import HttpResponseRedirect
 
 from apps.question.models import Question
@@ -8,7 +9,8 @@ from apps.question.models import Question
 class QuestionPermissionMixin(object):
     def dispatch(self, request, *args, **kwargs):
         id = self.kwargs.get('pk')
-        if self.request.user != Question.objects.get(id=id):
+        if self.request.user != Question.objects.get(id=id).poll.created_by:
             print('no te pertenece estooo!')
+            messages.add_message(request, messages.INFO, 'This question does not belong to you')
             return HttpResponseRedirect(reverse_lazy('poll:dashboard'))
         return super().dispatch(request, *args, **kwargs)
