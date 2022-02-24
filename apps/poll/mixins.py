bda9e96b31f1106d28edb9daeb6ca741742a1494
Fernando Guerrero <devrrior@gmail.com>
2022-02-24 09:41:29 -0600

    fix(poll/mixins.py): handle error if poll not found

--- apps/poll/mixins.py
+++ apps/poll/mixins.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.http.response import HttpResponseRedirect
@@ -8,9 +9,8 @@ from apps.poll.models import Poll
 class PollPermissionMixin(object):
     def dispatch(self, request, *args, **kwargs):
         id = self.kwargs.get('pk')
-        if self.request.user != Poll.objects.get(id=id).created_by:
-            print('no te pertenece estooo!')
-            print('user from self' + self.request.user)
+        poll_object = get_object_or_404(Poll, id=id)
+        if self.request.user != poll_object.created_by:
             messages.add_message(request, messages.INFO, 'This poll does not belong to you')
             return HttpResponseRedirect(reverse_lazy('poll:dashboard'))
         return super().dispatch(request, *args, **kwargs)
